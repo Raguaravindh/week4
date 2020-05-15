9906fe220ff9a1880bbb8ba8928bb32e1b1c9b12
RaguAravindh <64262524+Raguaravindh@users.noreply.github.com>
2020-05-15 17:44:00 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 (function (window) {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["John", "Jen", "Jason", "Paul", "Frank", "Larry"];
 
 
 for (var i = 0; i < names.length; i++) {
